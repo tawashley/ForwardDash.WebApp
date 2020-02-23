b7ddaf20ef5e2dcea9103d17771d69986208d0b0
Tristan Ashley <trisashley@gmail.com>
2020-02-23 20:08:17 +0000

    chore: git file name issues 2

---
+++ src/app.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+import { DateTimeWidget } from './widgets/dateTimeWidget/dateTime.widget'
+
+import './app.scss'
+
+const testQuery = gql`
+    query {
+        hello
+    }
+`
+
+function App() {
+    const { data } = useQuery(testQuery)
+
+    useEffect(() => {
+        if(data) {
+            console.log({ data })
+        }
+    }, [data])
+
+    return (
+        <main className="forward-dash-container">
+            <section className="forward-dash-content">
+                <DateTimeWidget />
+                <div>b</div>
+                <div>c</div>
+                <div>d</div>
+                <div>e</div>
+            </section>
+        </main>
+    );
+}
+export default App;
