b1b59e5f761f253902c5001d2c0e1740f89ad2e1
Tristan Ashley <trisashley@gmail.com>
2020-03-05 21:01:52 +0000

    feat: Few minor tweaks to forecast widget

--- src/widgets/weatherForecastWidget/weatherForecast.widget.tsx
+++ src/widgets/weatherForecastWidget/weatherForecast.widget.tsx
@@ -30,10 +30,9 @@ export const WeatherForecastWidget = () => {
                     {weeklyForecast.map((dayForecast, index) => {
                         return (
                             <li className="widget-weather-forecast__forecast-item" key={index}>
-                                <p>{dayForecast.condition.text}</p>
+                                <p className="widget-weather-forcecast__condition">{dayForecast.condition.text}</p>
                                 <img src={dayForecast.condition.iconSrc} alt={dayForecast.condition.text} />
                                 <div className="widget-weather-forecast__forest-info">
-                                    <p>{dayForecast.condition.text}</p>
                                     <p>Max: { dayForecast.maxTemperature.celsius } °C</p>
                                     <p>Min: { dayForecast.minTemperature.celsius } °C</p>
                                 </div>
